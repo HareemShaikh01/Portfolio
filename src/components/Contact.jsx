631408bbb15f3e463b4749e3b6764bb489a4a214
HareemShaikh01 <hareemshaikh2006@gmail.com>
2024-09-29 11:02:50 +0500

    lastttt

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -31,8 +31,8 @@ function Contact() {
 
     return (
         <div id='contact' className='text-white sm:p-10 p-4 border-2 border-[#21bd8e] rounded-t-[200px] mt-10'>
-            <h1 data-aos="fade-right" className='text-2xl md:text-4xl text-white font-bold text-center mt-10 mb-4'>Contact Me</h1>
-            <div className='flex flex-col-reverse md:flex-row gap-10 justify-around items-center py-10'>
+            <h1 data-aos="fade-right" className='text-2xl md:text-4xl text-white font-bold text-center mt-6'>Contact Me</h1>
+            <div className='flex flex-col-reverse md:flex-row gap-10 justify-around items-center py-6 md:py-10'>
                 <div className='max-w-72 flex flex-col gap-3 text-center md:text-left'>
                     <h1 data-aos="fade-right" className='text-2xl md:text-3xl font-semibold '>Drop me a message</h1>
                     <p data-aos="fade-left" className='text-sm text-[#b8c0bd]'>Reach out to discuss potential projects, collaborations, or any questions. I'm always open to new opportunities and connections.</p>
@@ -43,30 +43,30 @@ function Contact() {
                 </div>
 
                 <div>
-                    <form ref={formRef} onSubmit={sendEmail} className='flex flex-col gap-2 w-64 p-4 bg-[#197e60] text-sm items-center'>
+                    <form ref={formRef} onSubmit={sendEmail} className='flex flex-col gap-2 w-64 p-4 bg-[#197e60] text-sm items-center rounded-xl'>
                         <input
-                            className='bg-[#050511] p-2 rounded-lg w-full'
+                            className='bg-[#050511] p-2 rounded-lg w-full outline-none'
                             type="text"
                             name="from_name"
                             placeholder='Devid'
                             required
                         />
                         <input
-                            className='bg-[#050511] p-2 rounded-lg w-full'
+                            className='bg-[#050511] p-2 rounded-lg w-full outline-none'
                             type="email"
                             name="email" 
                             placeholder='dummy@gmail.com'
                             required
                         />
                         <textarea
-                            className='bg-[#050511] p-2 rounded-lg w-full'
+                            className='bg-[#050511] p-2 rounded-lg w-full outline-none'
                             name="message"
                             placeholder='Your Message Here...'
                             rows="6"
                             required
                         />
                         <input
-                            className='bg-[#050511] p-2 rounded-lg w-44 cursor-pointer hover:bg-[#050511d0]'
+                            className='bg-[#050511] p-2 rounded-lg w-44 cursor-pointer hover:bg-[#050511d0] outline-none'
                             type="submit"
                             value="Send"
                         />
