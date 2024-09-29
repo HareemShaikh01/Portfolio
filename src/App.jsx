bf3b02aee514e7310530bed77a09dd20e9071c9a
HareemShaikh01 <hareemshaikh2006@gmail.com>
2024-09-29 10:46:16 +0500

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   }, []);
   
   return (
-    <>
+    <div className="w-full overflow-x-hidden">
     <div>
       <div className="gradient w-64 h-64 fixed top-0 left-0 z-[-1] rounded-br-full"></div>
       <div className="gradient w-52 h-52 fixed bottom-0 right-0 z-[-1] rounded-tl-full "></div>
@@ -44,7 +44,7 @@ function App() {
     <HashLink to="#" smooth>
     <i className="uplinker fa-solid fa-angles-down fixed bottom-6 right-6 text-white text-2xl rotate-180"></i> 
       </HashLink>      
-    </>
+    </div>
   )
 }
 
